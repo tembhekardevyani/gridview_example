8cf2676025ca18d9ee3ba8f6f0fa3c102580c334
devyani <tembhekar.devyani8494@gmail.com>
2014-12-24 16:17:06 +0530

    this is my first project

---
+++ GridiedView/src/com/example/gridiedview/ImageAdapter.java
@@ -0,0 +1,69 @@
+package com.example.gridiedview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+	
+	private Context mContext;
+	
+	public ImageAdapter(Context context) {
+		mContext=context;
+	}
+
+	@Override
+	public int getCount() {
+		
+		
+		
+		return mThumbIds.length;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertview, ViewGroup parent) {
+		
+		ImageView imageview;
+		if(convertview==null){
+			imageview=new ImageView(mContext);
+			imageview.setLayoutParams(new GridView.LayoutParams(150,150));
+			imageview.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageview.setPadding(5, 5, 5, 5);
+		}else {
+			imageview=(ImageView) convertview;
+		}
+		imageview.setImageResource(mThumbIds[position]);
+		
+		return imageview;
+	}
+	
+	public Integer[] mThumbIds = {
+
+			R.drawable.sample_2, R.drawable.sample_3,
+       R.drawable.sample_4, R.drawable.sample_5,
+       R.drawable.sample_6, R.drawable.sample_7,
+       R.drawable.sample_0, R.drawable.sample_1,
+       R.drawable.sample_2, R.drawable.sample_3,
+       R.drawable.sample_4, R.drawable.sample_5,
+       R.drawable.sample_6, R.drawable.sample_7,
+       R.drawable.sample_0, R.drawable.sample_1,
+       R.drawable.sample_2, R.drawable.sample_3,
+       R.drawable.sample_4, R.drawable.sample_5,
+       R.drawable.sample_6, R.drawable.sample_7
+    };
+}
